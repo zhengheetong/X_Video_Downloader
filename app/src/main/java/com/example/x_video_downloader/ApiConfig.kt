0d2039d3b54a3a1d9aef1367a84c1a08df9e251d
zhengheetong <84259856+zhengheetong@users.noreply.github.com>
2026-03-01 18:04:04 +0800

    Update ApiConfig.kt

--- app/src/main/java/com/example/x_video_downloader/ApiConfig.kt
+++ app/src/main/java/com/example/x_video_downloader/ApiConfig.kt
@@ -2,6 +2,6 @@ package com.example.x_video_downloader
 
 object ApiConfig {
     // Your RapidAPI Credentials Vault
-    const val RAPID_API_KEY = "insert your own API Key here"
+    const val RAPID_API_KEY = "YOUR_API_KEY_HERE"
     const val RAPID_API_HOST = "twitter-video-downloader2.p.rapidapi.com"
 }
